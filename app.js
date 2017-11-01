04c97c360191881eb87c109963007395480481ce
mateusz <mateusz@kirmuc.pl>
2017-11-01 19:05:16 +0100

    Create initial model array and bind it with view

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
-function ViewModel() {
+let places = [{
+    name: "PKiN"
+  },
+  {
+    name: "Museum of Warsaw Uprising"
+  },
+  {
+    name: "National Museum of Art"
+  },
+  {
+    name: "Royal Palace"
+  },
+  {
+    name: "Belvedere"
+  }
+];
 
+let ViewModel = function() {
+  this.places = ko.observableArray(places);
 }
+ko.applyBindings(new ViewModel());
