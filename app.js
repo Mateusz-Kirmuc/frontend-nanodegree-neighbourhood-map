86d8c0ed182c193ee53ad7980ea47b4c99e9b680
mateusz <mateusz@kirmuc.pl>
2017-11-01 19:54:12 +0100

    Apply initial version of filtering

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-let places = [{
+const places = [{
     name: "PKiN"
   },
   {
@@ -17,5 +17,29 @@ let places = [{
 
 let ViewModel = function() {
   this.places = ko.observableArray(places);
+  this.query = ko.observable("");
+
+  this.resetPlaces = function() {
+    this.places(places);
+
+  };
+
+  this.filterPlaces = function() {
+    let filteredPlaces = [];
+    for (let place of places) {
+      if (place.name.search(this.query()) > -1) {
+        filteredPlaces.push(place);
+      }
+    }
+    this.places(filteredPlaces);
+    this.query("");
+  };
+  this.handleForm = function() {
+    if (!this.query()) {
+      this.resetPlaces();
+    } else {
+      this.filterPlaces();
+    }
+  };
 }
 ko.applyBindings(new ViewModel());
